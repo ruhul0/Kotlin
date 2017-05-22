197bf83ad8989aa817c23db81026d7a6def7e5bb
ruhul0 <ruhul20@gmail.com>
2017-05-22 21:46:59 +0600

    Initial commit

---
+++ src/Hello.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by ruhul on 5/22/17.
+ */
+fun main(args:Array<String>){
+    var a:Int=5
+    var b:Int=10
+    var m:String?
+    m="hello"
+    println(a+b)
+    println("Msg: "+m)
+}
