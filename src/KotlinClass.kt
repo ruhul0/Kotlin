4c08d7327323084d181529a7b92de9a4836ce920
ruhul0 <ruhul20@gmail.com>
2017-05-24 00:04:13 +0600

    Class, Constructor

--- src/KotlinClass.kt
+++ src/KotlinClass.kt
@@ -1,13 +1,40 @@
 /**
  * Created by ruhul on 5/23/17.
  */
-class KotlinClass(Name: String, ID: Int) {
-    init {
+class KotlinClass() {
+    var Name: String? = null
+    var ID: Int? = 0
+
+    constructor(Name: String, ID: Int) : this() {
+        fun getName(): String {
+            var name: String = this.Name!!
+            return name
+        }
+
+        fun getID(): Int {
+            var id: Int = this.ID!!
+            return id
+        }
+/*    init {
         println("Name: $Name\nID: $ID")
+        this.Name=Name
+
+        fun getName():String{
+            var name:String= this.Name!!
+            return name
+        }
+        fun getID():Int{
+            var id:Int = this.ID!!
+            return id
+        }*/
     }
 
 }
 
 fun main(args: Array<String>) {
     var student = KotlinClass("Ruhul", 306)
+    var name = student.Name
+    var id = student.ID
+    println("Name: $name\nID: $id")
+
 }
